7ea1d55f8c1eefcc40a3857d5e62f275963fa964
Benjamim Gomes <60727908+bejagomesvg@users.noreply.github.com>
2021-06-23 03:18:15 -0400

    Implementation Login & Security

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,22 @@
 import { useHistory } from "react-router-dom";
 
-import { auth, firebase } from "../services/firebase";
-
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
 
-import "../styles/auth.scss";
 import { Button } from "../components/Button";
+import { useAuth } from "../hooks/useAuth";
+
+import "../styles/auth.scss";
 
 export function Home() {
   const history = useHistory();
-
-  function handleCreateRoom() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-
-    auth.signInWithPopup(provider).then((result) => {
-      history.push("/rooms/new");
-    });
+  const { user, signInWithGoogle } = useAuth();
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle()
+    }
+    history.push("/rooms/new");
   }
 
   return (
